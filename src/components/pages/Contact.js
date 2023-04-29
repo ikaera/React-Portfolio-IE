ecb42ffe9859945014d1c8b8f0d10de6c34d4e8c
ikaera <ikaera@gmail.com>
2023-04-29 16:08:08 -0400

    edit Contact.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -25,11 +25,11 @@ export default function Contact() {
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
+    // Based on the input type, we set the state of either email, name, and password
     if (inputType === 'email') {
       setEmail(inputValue);
-    } else if (inputType === 'userName') {
-      setUserName(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
     } else {
       setPassword(inputValue);
     }
@@ -39,23 +39,21 @@ export default function Contact() {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
+    // First we check to see if the email is not valid or if the name is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Email or name is invalid');
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
       // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password for the account: ${userName}`,
-      );
-      return;
-    }
-    alert(`Hello ${userName}`);
+    // if (!checkPassword(password)) {
+    //   setErrorMessage(`Choose a more secure password for the account: ${name}`);
+    //   return;
+    // }
+    alert(`Hello ${name}`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
+    setName('');
     setPassword('');
     setEmail('');
   };
@@ -74,29 +72,54 @@ export default function Contact() {
           <div className="contact-form">
             <form className="form">
               <input
+                className="contact-half"
+                value={name}
+                name="name"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="Name"
+                required
+              />
+              <input
+                className="contact-half"
                 value={email}
                 name="email"
                 onChange={handleInputChange}
                 type="email"
-                placeholder="email"
+                placeholder="Email"
+                required
               />
+
               <input
-                value={userName}
-                name="userName"
-                onChange={handleInputChange}
+                className="contact-li"
+                placeholder="Subject"
                 type="text"
-                placeholder="username"
+                name="subject"
+                required
               />
-              <input
+              <textarea
+                className="contact-textarea"
+                placeholder="Message"
+                name="message"
+                required
+              ></textarea>
+
+              {/* <input
                 value={password}
                 name="password"
                 onChange={handleInputChange}
                 type="password"
                 placeholder="Password"
-              />
-              <button type="button" onClick={handleFormSubmit}>
+              /> */}
+              {/* <button type="button" onClick={handleFormSubmit}>
                 Submit
-              </button>
+              </button> */}
+              <input
+                type="submit"
+                className="contact-flat-button"
+                value="SEND"
+                onClick={handleFormSubmit}
+              />
             </form>
             {errorMessage && (
               <div>
